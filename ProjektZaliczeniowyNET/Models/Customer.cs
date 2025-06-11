dc6458c0391883ebae379faba4f1162c76d3fb57
Admin <Admin@DESKTOP-LC407O9>
2025-06-12 01:50:26 +0200

    Utworzenie modelu Customer.cs

--- ProjektZaliczeniowyNET/Models/Customer.cs
+++ ProjektZaliczeniowyNET/Models/Customer.cs
@@ -1,6 +1,61 @@
-namespace ProjektZaliczeniowyNET.Models;
+using System.ComponentModel.DataAnnotations;
 
-public class Customer
+namespace ProjektZaliczeniowyNET.Models
 {
-    
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(11)]
+        public string? TaxNumber { get; set; } // NIP dla firm
+
+        public bool IsCompany { get; set; } = false;
+
+        [StringLength(100)]
+        public string? CompanyName { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsActive { get; set; } = true;
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+
+        // Właściwości nawigacyjne
+        public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public virtual ICollection<ServiceOrder> ServiceOrders { get; set; } = new List<ServiceOrder>();
+
+        // Właściwość obliczana
+        public string FullName => $"{FirstName} {LastName}";
+        public string DisplayName => IsCompany && !string.IsNullOrEmpty(CompanyName) 
+            ? CompanyName 
+            : FullName;
+    }
 }

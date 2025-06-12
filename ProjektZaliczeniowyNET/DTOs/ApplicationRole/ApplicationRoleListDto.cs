d5dffe982089ef662f77ebe905f17cdff35517a3
Admin <Admin@DESKTOP-LC407O9>
2025-06-12 02:44:18 +0200

    Dto dla ApplicationRole

---
+++ ProjektZaliczeniowyNET/DTOs/ApplicationRole/ApplicationRoleListDto.cs
@@ -0,0 +1,10 @@
+namespace ProjektZaliczeniowyNET.DTOs.Role
+{
+    public class ApplicationRoleListDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ ProjektZaliczeniowyNET/DTOs/ApplicationRole/ApplicationRoleSelectDto.cs
@@ -0,0 +1,8 @@
+namespace ProjektZaliczeniowyNET.DTOs.Role
+{
+    public class ApplicationRoleSelectDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}

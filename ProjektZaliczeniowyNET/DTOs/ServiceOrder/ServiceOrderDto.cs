ad9027a3e5dc2da4f9ccb913c336d32fba5cb636
arnazell <andrzej_hubka@wp.pl>
2025-06-22 22:59:45 +0200

    dodatkowe informacje o samochodzie

--- ProjektZaliczeniowyNET/DTOs/ServiceOrder/ServiceOrderDto.cs
+++ ProjektZaliczeniowyNET/DTOs/ServiceOrder/ServiceOrderDto.cs
@@ -13,8 +13,10 @@ namespace ProjektZaliczeniowyNET.DTOs.ServiceOrder
         public int Id { get; set; }
         
         public CustomerDto Customer { get; set; }
-        //public IdentityUser Mechanic { get; set; }
+    
         public MechanicDto Mechanic { get; set; }
+        
+        public VehicleServiceOrderDetailsDto Vehicle { get; set; }
         
         public List<ServiceTaskCreateDto> ServiceTasks { get; set; } = new();
         public Models.ServiceOrderStatus Status { get; set; } = Models.ServiceOrderStatus.Pending;
---
+++ ProjektZaliczeniowyNET/DTOs/Vehicle/VehicleServiceOrderDetailsDto.cs
@@ -0,0 +1,7 @@
+namespace ProjektZaliczeniowyNET.DTOs.Vehicle;
+
+public class VehicleServiceOrderDetailsDto
+{
+    public string DisplayName { get; set; } = null!;
+    public string LicensePlate { get; set; } = null!;
+}

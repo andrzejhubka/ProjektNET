d1c08da03031ae935318d47f2511ce106deac7a7
arnazell <andrzej_hubka@wp.pl>
2025-06-24 09:44:55 +0200

    naprawa bledu

--- ProjektZaliczeniowyNET/Controllers/AccountController.cs
+++ ProjektZaliczeniowyNET/Controllers/AccountController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
-using ProjektZaliczeniowyNET.Constants;
 using ProjektZaliczeniowyNET.ViewModels;
 using ProjektZaliczeniowyNET.Models;
 using ProjektZaliczeniowyNET.Services;
@@ -10,121 +9,127 @@ using ProjektZaliczeniowyNET.Services;
 namespace ProjektZaliczeniowyNET.Controllers
 {
     public class AccountController : Controller
-{
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
-    private readonly IEmailSender _emailSender;
-
-    public AccountController(
-        UserManager<ApplicationUser> userManager, 
-        SignInManager<ApplicationUser> signInManager, 
-        IEmailSender emailSender)
     {
-        _userManager = userManager;
-        _signInManager = signInManager;
-        _emailSender = emailSender;
-    }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
 
-    [HttpGet]
-    public IActionResult Register() => View();
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Register(RegisterViewModel model)
-    {
-        if (!ModelState.IsValid)
-            return View(model);
-
-        var user = CreateApplicationUser(model);
-        var result = await _userManager.CreateAsync(user, model.Password);
-        
-        if (result.Succeeded)
-            return await HandleSuccessfulRegistration(user, model.Email);
-
-        AddErrorsToModelState(result.Errors);
-        return View(model);
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
-    {
-        if (!ModelState.IsValid)
-            return View(model);
-
-        var user = await _userManager.FindByEmailAsync(model.Email);
-        
-        if (!await IsUserEmailConfirmed(user))
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,  IEmailSender emailSender)
         {
-            ModelState.AddModelError(string.Empty, ErrorMessages.EmailNotConfirmed);
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser 
+                { 
+                    UserName = model.Email, 
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // Generowanie tokenu potwierdzającego email
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            
+                    // Tworzenie linku potwierdzającego
+                    var confirmationLink = Url.Action("ConfirmEmail", "Account",
+                        new { userId = user.Id, token = token }, Request.Scheme);
+            
+                    // Wysyłanie emaila z linkiem potwierdzającym
+                    await _emailSender.SendEmailAsync(model.Email, "Potwierdź swój email",
+                        $"Kliknij <a href='{confirmationLink}'>tutaj</a> aby potwierdzić swój email.");
+                    
+                    // Przekieruj do strony informującej o wysłaniu emaila
+                    return View("EmailConfirmationSent");
+                }
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+        
+        [HttpGet]
+        public IActionResult Login() => View();
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError(string.Empty, 
+                            "Musisz potwierdzić swój adres email przed zalogowaniem. Sprawdź swoją skrzynkę pocztową.");
+                        return View(model);
+                    }
+                }
+                
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                    return RedirectToLocal(returnUrl);
+
+                ModelState.AddModelError(string.Empty, "Nieprawidłowa próba logowania.");
+            }
+
             return View(model);
         }
 
-        var result = await _signInManager.PasswordSignInAsync(
-            model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-
-        if (result.Succeeded)
-            return RedirectToLocal(returnUrl);
-
-        ModelState.AddModelError(string.Empty, ErrorMessages.InvalidLogin);
-        return View(model);
-    }
-
-    public async Task<IActionResult> ConfirmEmail(string userId, string token)
-    {
-        if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(token))
-            return View(ViewNames.Error);
-
-        var user = await _userManager.FindByIdAsync(userId);
-        if (user == null)
-            return View(ViewNames.Error);
-
-        var result = await _userManager.ConfirmEmailAsync(user, token);
-        return View(result.Succeeded ? ViewNames.EmailConfirmed : ViewNames.Error);
-    }
-
-    // Private helper methods
-    private ApplicationUser CreateApplicationUser(RegisterViewModel model)
-    {
-        return new ApplicationUser
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
         {
-            UserName = model.Email,
-            Email = model.Email,
-            FirstName = model.FirstName,
-            LastName = model.LastName,
-            PhoneNumber = model.PhoneNumber
-        };
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+        
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(token))
+            {
+                return View("Error");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return View("EmailConfirmed");
+            }
+            return View("Error");
+        }
     }
-
-    private async Task<IActionResult> HandleSuccessfulRegistration(ApplicationUser user, string email)
-    {
-        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-        var confirmationLink = Url.Action("ConfirmEmail", "Account",
-            new { userId = user.Id, token }, Request.Scheme);
-
-        await _emailSender.SendEmailAsync(email, "Potwierdź swój email",
-            $"Kliknij <a href='{confirmationLink}'>tutaj</a> aby potwierdzić swój email.");
-
-        return View(ViewNames.EmailConfirmationSent);
-    }
-
-    private async Task<bool> IsUserEmailConfirmed(ApplicationUser user)
-    {
-        return user != null && await _userManager.IsEmailConfirmedAsync(user);
-    }
-
-    private void AddErrorsToModelState(IEnumerable<IdentityError> errors)
-    {
-        foreach (var error in errors)
-            ModelState.AddModelError(string.Empty, error.Description);
-    }
-
-    private IActionResult RedirectToLocal(string returnUrl)
-    {
-        return Url.IsLocalUrl(returnUrl) 
-            ? Redirect(returnUrl) 
-            : RedirectToAction("Index", "Home");
-    }
-}
-
 }
